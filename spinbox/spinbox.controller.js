372f1c4f03cc07c921432cc311045af817b6ffeb
Kimchunyong <lovemy5854@naver.com>
2018-11-21 11:58:40 +0900

    prototype 위치 변경

--- spinbox/spinbox.controller.js
+++ spinbox/spinbox.controller.js
@@ -8,15 +8,15 @@
         $(".btn-increase").on('click', $.proxy(this.onClickIncrease, this));
         $(".btn-decrease").on('click', $.proxy(this.onClickDecrease, this));
     }
+    SpinboxController.prototype = {
+        onClickIncrease: function () {
+            this.spinboxModel.increase();
+            this.spinboxView.render(this.spinboxModel.getData());
+        },
+        onClickDecrease: function () {
+            this.spinboxModel.decrease();
+            this.spinboxView.render(this.spinboxModel.getData());
+        }
+    }
     exports.SpinboxController = SpinboxController;
 })(this);
-SpinboxController.prototype = {
-    onClickIncrease: function () {
-        this.spinboxModel.increase();
-        this.spinboxView.render(this.spinboxModel.getData());
-    },
-    onClickDecrease: function () {
-        this.spinboxModel.decrease();
-        this.spinboxView.render(this.spinboxModel.getData());
-    }
-}
--- spinbox/spinbox.model.js
+++ spinbox/spinbox.model.js
@@ -3,21 +3,21 @@
         value = value || 100;
         this.data = value;
     }
+    SpinboxModel.prototype = {
+        increase: function (value) {
+            value = value || 1;
+            this.data += value;
+            return this.data;
+        },
+        decrease: function (value) {
+            value = value || 1;
+            this.data -= value;
+            return this.data;
+        },
+        getData: function () {
+            return this.data;
+        }
+    }
     exports.SpinboxModel = SpinboxModel;
 })(this);
 
-SpinboxModel.prototype = {
-    increase: function (value) {
-        value = value || 1;
-        this.data += value;
-        return this.data;
-    },
-    decrease: function (value) {
-        value = value || 1;
-        this.data -= value;
-        return this.data;
-    },
-    getData: function () {
-        return this.data;
-    }
-}
